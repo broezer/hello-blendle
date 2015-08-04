2279ae4b24f11fbc4c3605c81009cae6023e1fcd
broezer@gmail.com <broezer@gmail.com>
2015-08-04 13:43:11 +0200

    Update package.son & gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,31 @@ var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var ignore = require('gulp-ignore');
+var gutil = require('gulp-util');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 
-gulp.task('serve',['sass'], function() {
+gulp.task('serve',['sass', 'compress'], function() {
     browserSync.init({
         server: "."
     });
     gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['compress']);
     gulp.watch('**/*.html').on('change', browserSync.reload);
     gulp.watch('src/**/*.scss').on('change', browserSync.reload);
+    gulp.watch('src/**/*.js').on('change', browserSync.reload);
 });
 
+gulp.task('compress', function() {
+  return gulp.src('src/js/**.js')
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('sass', function () {
     sass('src/scss/index.scss', {sourcemap: true, style: 'compact'})
@@ -27,5 +37,7 @@ gulp.task('sass', function () {
         .pipe(rename('app.css'))
         .pipe(gulp.dest('dist'));
 });
+
+
 
 gulp.task('default', ['serve']);
